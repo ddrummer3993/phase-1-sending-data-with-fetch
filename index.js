3114d09bb65ec9184e11a470c849d5e30687ca53
David Drummer <david.b.drummer@gmail.com>
2022-02-21 12:32:11 -0500

    commit tree ef748afeab2d66875eb77671c0b27798ca0e19db

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// create function submitData
+function submitData(string1, string2) {
+    const userDataObj = {
+        name: string1,
+        email: string2
+    };
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userDataObj)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        appendToDom(data)
+    })
+    .catch(error => errorDom(error));
+};
+
+//submitData('Dave', 'fakeemail@whocares.com');
+
+//create appendToDom fuinction 
+function appendToDom (user) {
+    let post = document.createElement('h1');
+    post.innerHTML = `${user.id}`;
+    console.log(`${user.id}`);
+    console.log(document.querySelector('h1'));
+    return document.querySelector('body').appendChild(post);
+};
+
+//create errorDom function
+function errorDom (error) {
+    let errorMessage = document.createElement('h1');
+    errorMessage.innerHTML = `${error.message}`;
+    return document.querySelector('body').appendChild(errorMessage);
+};
+
+
